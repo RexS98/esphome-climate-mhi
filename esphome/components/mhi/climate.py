ed9d726df763659deae525cb9d6ad7c95a447a16
Rex <36916438+RexS98@users.noreply.github.com>
2025-07-27 23:00:40 +0800

    Update climate.py
    Fix no entity created after ESPHome update to 2025.7

--- esphome/components/mhi/climate.py
+++ esphome/components/mhi/climate.py
@@ -1,19 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
+
 
 AUTO_LOAD = ['climate_ir']
 
 mhi_ns = cg.esphome_ns.namespace('mhi')
 MhiClimate = mhi_ns.class_('MhiClimate', climate_ir.ClimateIR)
 
-#CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend({
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(MhiClimate),
-})
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(MhiClimate).extend(
+    {
+    	cv.GenerateID(): cv.declare_id(MhiClimate),
+}) 	
 
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield climate_ir.register_climate_ir(var, config)
+async def to_code(config):
+    await climate_ir.new_climate_ir(config)
